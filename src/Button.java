b177b36734b6c02b816a5ff777fe56eff746d20f
Regina <rsimonenko012@gmail.com>
2025-11-17 23:04:28 +0700

    feat: add Bell and OddEvenSeparator classes (tasks 3-4)

--- src/Button.java
+++ src/Button.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Button {
+    private int clickCount;
+
+    public Button() {
+        this.clickCount = 0;
+    }
+
+    public void click() {
+        clickCount++;
+        System.out.println("Количество нажатий: " + clickCount);
+    }
+
+    // Геттер для тестирования
+    public int getClickCount() {
+        return clickCount;
+    }
 }
