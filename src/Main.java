c85ae640523230ca48f0ce489e324eb1e8edb67f
Regina <rsimonenko012@gmail.com>
2025-11-29 13:29:45 +0700

    feat: add base project structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-
+import java.sql.*;
 
 public class Main {
 
+    private static final String URL = "jdbc:h2:mem:lab9db;DB_CLOSE_DELAY=-1";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
+
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            System.out.println("Подключение к H2 установлено!\n");
+
+        } catch (Exception e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+    }
 }
